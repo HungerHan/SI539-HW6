5f2e386523f8562055da0ec268d0136d2c903973
ZihanZhan <30337729+HungerHan@users.noreply.github.com>
2020-11-16 21:44:26 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,10 +5,60 @@ window.addEventListener("load", function() {
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+    console.log("Play Video");
+    video.play();
+
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+    console.log("Pause Video");
+    video.pause();
+
 });
 
+document.querySelector("#slower").addEventListener("click", function () {
+    console.log("Slow Down");
+    video.playbackRate  *= 0.9;
+    console.log("New Speed:", video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+    console.log("Speed Up");
+    video.playbackRate  *= 1.1;
+    console.log("New Speed:", video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+    console.log("Skip Ahead");
+    video.currentTime += 5;
+    if (video.currentTime >= video.duration) {
+        video.currentTime = 0;
+        video.play();
+    }
+    console.log("Video is at:", video.currentTime)
+});
+
+document.querySelector("#mute").addEventListener("click", function () {
+    video.muted = !video.muted;
+    document.querySelector("#mute").innerHTML = !video.muted ? "Mute" : "Unmute";
+    console.log(video.muted ? "Mute" : "Unmute");
+});
+
+document.querySelector("#volumeSlider").addEventListener("click", function () {
+    console.log("Volume Slider");
+    document.querySelector("#volume").innerHTML = document.querySelector("#volumeSlider").value + "%";
+    video.volume = document.querySelector("#volumeSlider").value / 100.0;
+    console.log("New Volume:", document.querySelector("#volumeSlider").value + "%");
+});
+
+document.querySelector("#old").addEventListener("click", function () {
+    console.log("Old School")
+    video.classList.add("oldTime");
+
+});
+
+document.querySelector("#original").addEventListener("click", function () {
+    console.log("Original")
+    video.classList.remove("oldTime");
+
+});
